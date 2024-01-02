f2d029a2508796c9be573d891ca9b5976221c34b
Md Sha Newaz <sawonnstu@gmail.com>
2024-01-02 22:53:58 +0600

    Deploy

--- EventRegProj/settings.py
+++ EventRegProj/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-f!+wv7k7q6cx!=*y^@hdd)go#k+z+$%^nsfwzgn@n166v7vp@w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
